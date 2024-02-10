9f42743d4a5ac7867c7a2d928bc4b0688633b760
Eroun-Harry <jhlee@eroun.ai>
2024-02-10 13:34:47 +0900

    Add: Print all users info in admin page

--- client/src/components/Admin/Users.tsx
+++ client/src/components/Admin/Users.tsx
@@ -1,9 +1,35 @@
+import { useGetAllUsersQuery } from 'librechat-data-provider/react-query';
+
 export default function Users() {
+  const usersQuery = useGetAllUsersQuery();
+
+  if (usersQuery.isError) {
+    const error = usersQuery.error;
+    if (error instanceof Error) {
+      return <div>Error: {error.message}</div>;
+    }
+    return <div>Error</div>;
+  }
+
+  if (usersQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="text-token-text-primary flex flex-col gap-2 pb-2 text-sm">
-      <div>
-        <span>여기에 사용자 관리 페이지가 들어갈 예정입니다.</span>
-      </div>
+    <div className="text-token-text-primary gap-2 pb-2 text-sm">
+      {usersQuery.data?.userList.map((user) => (
+        <div className="grid" key={user.username}>
+          <span>Name: {user.name}</span>
+          <span>Username: {user.username}</span>
+          <span>Email: {user.email}</span>
+          <span>Role: {user.role}</span>
+          <span>Provider: {user.provider}</span>
+          <span>Plugins: {user.plugins.join(', ')}</span>
+          <span>CreatedAt: {user.createdAt}</span>
+          <span>UpdatedAt: {user.updatedAt}</span>
+          <span>====================</span>
+        </div>
+      ))}
     </div>
   );
 }
